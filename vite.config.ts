591a09769f7d1c1a0c9a8e3e74bd1312ed27ceff
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-02-18 00:46:09 +0300

    added host option in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  server: {
+    host: true,
+  },
 });
